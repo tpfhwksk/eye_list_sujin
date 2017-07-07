1afabaef2f772144b70c6a3ce3cd85d279ef4b0f
eyexpo <aaronzuo@eyexpo.com>
2017-07-07 10:59:59 -0700

    add stereo and video file

--- list_up/list_up/MetalScope.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for MetalScope.
-FOUNDATION_EXPORT double MetalScopeVersionNumber;
-
-//! Project version string for MetalScope.
-FOUNDATION_EXPORT const unsigned char MetalScopeVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <MetalScope/PublicHeader.h>
